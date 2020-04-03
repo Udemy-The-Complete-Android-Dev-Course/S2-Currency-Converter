66e04eb078a5273d96c32a699e3de58dd4e1ae56
Laura Dansorean <9722740+ldansorean@users.noreply.github.com>
2020-04-03 21:07:04 +0100

    Implement the most basic and useless currency converter in the world.

--- app/src/main/java/com/example/s2currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/s2currencyconverter/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.s2currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static Double EXCHANGE_RATE = 1.23;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convert(View view) {
+        EditText amountEditText = findViewById(R.id.amount);
+        try {
+            Double amount = Double.parseDouble(amountEditText.getText().toString());
+            Double usd = amount * EXCHANGE_RATE;
+            Toast.makeText(getApplicationContext(), usd + "$", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Invalid pounds amount. Must be a number.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
